35111dd54736079a3622d6a5edac99cdd6f2a264
Steve Fallet <steve.fallet@divtec.ch>
2022-05-18 16:22:34 +0200

    nettoyage du header

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -1,25 +1,15 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="hHh lpr fFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
-        <q-toolbar-title>
-          Quasar App
+        <q-toolbar-title class="absolute-center">
+          ToDo
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
     <q-drawer
+      breakpoint="767"
       v-model="leftDrawerOpen"
       show-if-above
       bordered
@@ -91,11 +81,16 @@ export default {
         }
       ]
     }
-  },
-  methods: {
-    toggleLeftDrawer () {
-      this.leftDrawerOpen = !this.leftDrawerOpen
-    }
   }
 }
 </script>
+
+<style>
+/* Applique les règles de ce bloc uniquement aux écrans >= 768px */
+@media screen and (min-width: 768px) {
+  /* Cache les éléments avec la classe CSS q-footer */
+  .q-footer {
+    display: none;
+  }
+}
+</style>
